74d2d7104f26a37b250f8c07a010719f1e6eb15b
Valentine Miranda <valentine.miranda@grayquest.com>
2024-02-02 12:31:26 +0530

    optimised loader

--- Base Classes/GQViewController.swift
+++ Base Classes/GQViewController.swift
@@ -13,23 +13,28 @@ public class GQViewController: UIViewController {
     
     public func showLoader() {
         DispatchQueue.main.async {
-            let activityIndicator = UIActivityIndicatorView()
-            activityIndicator.hidesWhenStopped = true
-            activityIndicator.color = .black
-            activityIndicator.backgroundColor = .black.withAlphaComponent(0.3)
-            self.view.addSubview(activityIndicator)
+            if let loader = self.loader {
+                loader.startAnimating()
+                loader.isHidden = false
+            } else {
+                let activityIndicator = UIActivityIndicatorView()
+                activityIndicator.hidesWhenStopped = true
+                activityIndicator.color = .black
+                activityIndicator.backgroundColor = .black.withAlphaComponent(0.3)
+                self.view.addSubview(activityIndicator)
 
-            //Adding Constraints
-            activityIndicator.translatesAutoresizingMaskIntoConstraints = false
-            activityIndicator.center = self.view.center
-            activityIndicator.heightAnchor.constraint(equalToConstant: 40).isActive = true
-            activityIndicator.widthAnchor.constraint(equalToConstant: 40).isActive = true
-            activityIndicator.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-            activityIndicator.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+                //Adding Constraints
+                activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+                activityIndicator.center = self.view.center
+                activityIndicator.heightAnchor.constraint(equalToConstant: 40).isActive = true
+                activityIndicator.widthAnchor.constraint(equalToConstant: 40).isActive = true
+                activityIndicator.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+                activityIndicator.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
 
-            activityIndicator.startAnimating()
-            self.view.addSubview(activityIndicator)
-            self.loader = activityIndicator
+                activityIndicator.startAnimating()
+                self.view.addSubview(activityIndicator)
+                self.loader = activityIndicator
+            }
             
             // disable interaction
             self.view.isUserInteractionEnabled = false
